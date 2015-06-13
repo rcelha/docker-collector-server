58ed9330c3496509114328ce38c33a26c6f42b69
rcelha <rcelha@gmail.com>
2015-06-13 20:21:44 -0300

    first

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+#!coding=utf-8
+
+import click
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+
+@app.route("/container_stat", methods=['GET', 'POST'])
+def stat():
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        logging.warn('data received')
+        logging.warn(data)
+        return 'ok'
+    return "ahn?"
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def run():
+    app.run(host='0.0.0.0')
+
+cli.add_command(run)
+
+if __name__ == '__main__':
+    cli()
